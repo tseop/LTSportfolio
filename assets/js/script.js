e2f91b75c603fdd03880d784f1c1d5b39a729a6c
koreaseop <leetsseop@naver.com>
2024-03-25 04:06:19 +0900

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,9 +72,20 @@ function setScrollAnimate(){
     });
 }
 
+// 모바일 브라우저 오류 해결
+function setMoHeight(){
+    let vh = 0;
+
+    useEffect(() => {
+      vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, []);
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     setFixedHeader();
     setAllMenu();
     setTabs();
     setScrollAnimate();
+    setMoHeight();
 });
